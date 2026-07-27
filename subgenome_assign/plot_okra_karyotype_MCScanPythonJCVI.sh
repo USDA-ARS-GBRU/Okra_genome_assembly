1e8e3c155be747c66c468b008aaae25bb8a871cd
Ash Yow <45609354+agyow@users.noreply.github.com>
2026-07-27 15:12:49 -0400

    update and move karyotype plotting script

---
+++ subgenome_assign/plot_okra_karyotype_MCScanPythonJCVI.sh
@@ -0,0 +1,4 @@
+# Changed alpha (transparency) parameter in karyotype python script to 0.35.
+
+# Generate karyotype plot
+python -m jcvi.graphics.karyotype seqids_file layout_file --basepair --nocircles --chrstyle roundrect --dpi 600 --font Arial
