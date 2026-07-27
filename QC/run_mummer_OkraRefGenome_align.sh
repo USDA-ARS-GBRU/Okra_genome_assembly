4bd086c52743a9dcb84adfce86866636c994a8dd
Ash Yow <45609354+agyow@users.noreply.github.com>
2026-07-27 15:14:55 -0400

    Update and move nucmer command with new input file

--- QC/run_mummer_OkraRefGenome_align.sh
+++ QC/run_mummer_OkraRefGenome_align.sh
@@ -8,7 +8,7 @@ module load mummer
 
 nucmer -V 
 
-nucmer --prefix=Okra_ClemsonSpineless_FINAL_vs_ref --maxgap=500 --mincluster=100 --threads=40 /project/gbru_okra/AshYow_Okra_genome/ragtag_run3_withCanu/Canu_patch_RAGTAGrun1-GBRUselectScaffs/added_Canu_tigs_with_telomeres/HiC_edits/LR_gapcloser_output/ragtag_output/Okra_ClemsonSpineless_FINAL.sorted.excludePGAscaf918.fa /project/gbru_okra/Assembly/PublishedReferences/GWHBWBG00000000.genome.fasta 
+nucmer --prefix=Okra_ClemsonSpineless_FINAL_vs_ref --maxgap=500 --mincluster=100 --threads=40 Okra_ClemsonSpineless.fa /project/gbru_okra/Assembly/PublishedReferences/GWHBWBG00000000.genome.fasta 
 
 
 delta-filter -g -l 5000 -i 99 -u 99 Okra_ClemsonSpineless_FINAL_vs_ref.delta > Okra_ClemsonSpineless_FINAL_vs_ref_l5000i99u99.delta
